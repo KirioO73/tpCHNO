035b52341dceed9217e7b9574058fdce2c48938d
IDreDD <apfedcup@gmail.com>
2018-11-08 23:33:13 +0100

    permutation

--- GenereEtTeste.cpp
+++ GenereEtTeste.cpp
@@ -2,9 +2,24 @@
 #include <iostream>
 #include "./include/graphReader.h"
 #include "./include/Graph.h"
+#include "include/Parcours.h"
 
 
 using namespace std;
+
+std::vector<std::string> permutations (std::vector<std::string> head, std::vector<std::string> tail)
+{
+    if (head.size()==0){
+        return tail;
+    }else
+        for (int i = 0; i<head.size(); i++)
+        {
+            tail.push_back(head[i]);
+            head.erase(head.begin()+i);
+            permutations( head, tail);
+        }
+}
+
 int main( int argc, char** argv ){
     if(argc < 2){
         cout << "Usage: genereEtTest <inputGraph.txt>" << endl;
@@ -21,6 +36,10 @@ int main( int argc, char** argv ){
     for (int i =0; i< G.getNbLink(); i++){
         cout << G.getLinks().at(i).getS1() << "," << G.getLinks().at(i).getS2() << endl;
     }
+
+
+
+
 
 
     return 0;
